4964848c94cd03a60ff58e54b88a67b5330c2ecc
trvrl <trev.luu@gmail.com>
2017-10-20 19:13:06 -0700

    Implemented basic turn structure.

--- battleship.pl
+++ battleship.pl
@@ -1,13 +1,75 @@
-main(X) :-
-    open('./board.txt', read, B),
-    readStream(B, X).
+:- dynamic game_won/0.
 
+%% Runs the game
 run :-
-    open('./board.txt', read, B),
-    readStream(B, X),
-    write(X),
+    start_game,
+    play_game,
+    end_game.
+
+%% Initializes the game
+start_game :-
+    write('Welcome to Battleship'),
     nl,
-    write('Welcome to Battleship!'), nl.
+    write('Enter \'Y.\' to start...'),
+    nl,
+    read(Yes),
+    (Yes = 'Y' ; Yes = 'y').
+
+%% Starts the play sequence
+play_game :-
+    write('Let\'s play!'),
+    nl,
+    open('./board.txt', read, Stream),
+    readStream(Stream, Board),
+    assert( board(player_primary, Board) ),
+    empty_board(EB),
+    assert( board(player_tracking, EB) ),
+    player_turn.
+
+%% Ends the game
+end_game :-
+    write('Thanks for Playing!'),
+    nl,
+    retract( game_won ).
+
+%% Reads the move of a player
+read_move :-
+    write('Make your next move!'), nl,
+    read(_).
+
+validate_move :-
+    write('Your move was valid!'), nl.
+
+update_boards.
+
+check_win :-
+    write('Wanna win?'), nl,
+    read(X),
+    X = 'Y',
+    assert( game_won ).
+
+player_turn :- game_won.
+
+player_turn :-
+    \+ game_won,
+    write('Player\'s turn.'),
+    read_move,
+    validate_move,
+    check_win,
+    update_boards,
+    computer_turn.
+
+computer_turn :- game_won.
+
+computer_turn :-
+    \+ game_won,
+    write('Computer\'s turn.'),
+    read_move,
+    validate_move,
+    check_win,
+    update_boards,
+    player_turn.
+
 
 % Reads a single list from the board file.
 readStream(Stream, Result) :-
@@ -62,3 +124,12 @@ canAttempt(Coord, B) :-
     toIndex(Coord, Index),
     get(B, Index, Space),
     \+ attempted(Space).
+
+empty_board(EB) :-
+    EB = [
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0
+    ].
