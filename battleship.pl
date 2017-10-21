343f6f2f45af60ddd7d0990f87a40448704cc8a1
Tak Poon <takpoon@Taks-MacBook-Pro.local>
2017-10-21 00:48:15 -0700

    Adds spacesOccupied, sublist, getRow, getColumn, countShips and validateBoard functions. Small modifications to run function.

--- battleship.pl
+++ battleship.pl
@@ -6,8 +6,71 @@ run :-
     open('./board.txt', read, B),
     readStream(B, X),
     write(X),
+    validateBoard(X),
+    !
+    .
+
+validateBoard(B) :-
+    length(B,25),
+    spacesOccupied(B,6),
+    occupiedValid(B,3),
     nl,
-    write('Welcome to Battleship!'), nl.
+    write('Welcome to Battleship!');
+    nl,
+    write("Board invalid. Please try again.").
+
+occupiedValid(B,NST) :-
+    getRow(0,B,R0),
+    countShips(R0,CR0,0,NS1),
+    getRow(1,B,R1),
+    countShips(R1,CR1,NS1,NS2),
+    getRow(2,B,R2),
+    countShips(R2,CR2,NS2,NS3),
+    getRow(3,B,R3),
+    countShips(R3,CR3,NS3,NS4),
+    getRow(4,B,R4),
+    countShips(R4,CR4,NS4,NS5),
+    append(CR0,CR1,CRN1),
+    append(CRN1,CR2,CRN2),
+    append(CRN2,CR3,CRN3),
+    append(CRN3,CR4,CRN4),
+    getColumn(0,CRN4,C0),
+    countShips(C0,_,NS5,NS6),
+    getColumn(1,CRN4,C1),
+    countShips(C1,_,NS6,NS7),
+    getColumn(2,CRN4,C2),
+    countShips(C2,_,NS7,NS8),
+    getColumn(3,CRN4,C3),
+    countShips(C3,_,NS8,NS9),
+    getColumn(4,CRN4,C4),
+    countShips(C4,_,NS9,NS10),
+    NS10 = NST.
+
+countShips([],[],A,A).
+countShips([H|T],[H|CR],A,N) :- dif(H,1),countShips(T,CR,A,N).
+countShips([1],[1],A,A).
+countShips([1,0|T],[1,0|CR],A,N) :- countShips(T,CR,A,N).
+countShips([1,1,1|T],[1,0,0|CR],A,N) :- A1 is A+1, countShips(T,CR,A1,N).
+countShips([1,1,1|T],[0,0,1|CR],A,N) :- A1 is A+1, countShips(T,CR,A1,N).
+countShips([1,1,1|T],[1,1,1|CR],A,N) :- countShips(T,CR,A,N).
+countShips([1,1|T],[0,0|CR],A,N) :- A1 is A+1, countShips(T,CR,A1,N).
+
+getColumn(N,_,[]) :- N>24.
+getColumn(N,B,R) :-
+    nth0(N,B,A), N1 is N+5, getColumn(N1,B,R1), R = [A|R1].
+
+getRow(N,B,R) :-
+    N1 is N*5, N2 is N1+5, sublist(R,N1,N2,B).
+
+sublist(L,S,E,[_|T]):- 
+    S>0, S<E, sublist(L,S-1,E-1,T).
+sublist(L,S,E,[H|T]):- 
+    0 is S, S<E, E2 is E-1, L=[H|T1], sublist(T1,0,E2,T).
+sublist([],0,0,_).
+
+spacesOccupied([],0).
+spacesOccupied([S|B],N) :- S = 1, spacesOccupied(B,N1), N is N1+1.
+spacesOccupied([S|B],N) :- S \= 1, spacesOccupied(B,N).
 
 % Reads a single list from the board file.
 readStream(Stream, Result) :-
